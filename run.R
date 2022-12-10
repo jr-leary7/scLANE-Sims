ca331eb9a7484f34046f5501ce9e87607bc11525
John Leary <j.leary@login1.ufhpc>
2022-12-10 14:28:00 -0500

    Running scLANE GLMM sims -- commented out tradeSeq / other scLANE sims for now

--- run.R
+++ run.R
@@ -1,4 +1,6 @@
+# command: sbatch -t 80:00:00 -c 6 --mem=180G -J sim --account=biostat-dept --qos=biostat-dept-b --wrap="module load R; Rscript run.R"
+
 setwd("/blue/rbacher/j.leary/repos/scLANE-Sims")
 library(targets)
 library(tarchetypes)
-tar_make_future(workers = 6)
+tar_make_future(workers = 10)
