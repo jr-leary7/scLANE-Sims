4a2e6ba256fe384407d08b37c7ea657ce593c753
John Leary <j.leary@login2.ufhpc>
2022-12-12 03:47:58 -0500

    GLMM sim analysis for aarons class

--- run.R
+++ run.R
@@ -1,4 +1,4 @@
-# command: sbatch -t 80:00:00 -c 6 --mem=180G -J sim --account=biostat-dept --qos=biostat-dept-b --wrap="module load R; Rscript run.R"
+# command: sbatch -t 80:00:00 -c 6 --mem=180G -J sim --account=biostat-dept --qos=biostat-dept-b --wrap="module load R; Rscript run.R" -- 3 days, 8h total runtime possible
 
 setwd("/blue/rbacher/j.leary/repos/scLANE-Sims")
 library(targets)
